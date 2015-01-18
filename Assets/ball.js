01bc54ac7b59f393519c607ee9f564dd8b4dc32e
kirisanth <kirisanth.s@gmail.com>
2015-01-17 21:17:09 -0500

    Initial Commit with myo integration

---
+++ Assets/ball.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+function Start () {
+	// Debug.Log('TEST');
+	// Debug.Log(Object);
+	// print(gameObject.name);	
+}
+
+function Update () {
+
+
+}
+
+function onCollisionEnter(collision : Collision) {
+	Debug.Log("Collision Detected");
+}
+
+function onTriggerEnter(collision : Collision) {
+	Debug.Log("Passing through Detected");
+}
---
+++ Assets/plane.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class plane : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.WakeUp();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision collisionInfo)
+	{
+	    print("Detected collision between " + gameObject.name + " and " + collisionInfo.collider.name);
+	    print("There are " + collisionInfo.contacts.Length + " point(s) of contacts");
+	    print("Their relative velocity is " + collisionInfo.relativeVelocity);
+	}
+
+	void OnCollisionStay(Collision collisionInfo)
+	{
+	    print(gameObject.name + " and " + collisionInfo.collider.name + " are still colliding");
+	}
+
+	void OnCollisionExit(Collision collisionInfo)
+	{
+	    print(gameObject.name + " and " + collisionInfo.collider.name + " are no longer colliding");
+	}
+
+}
